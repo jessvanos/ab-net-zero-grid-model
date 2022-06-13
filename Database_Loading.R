186c5e230cb7b9fb4f7c3e4a34242b5f21fa33fc
Jessica Van Os <jessvanos@gmail.com>
2022-06-13 14:25:38 -0600

    Update

--- Database_Loading.R
+++ Database_Loading.R
@@ -6,6 +6,18 @@
 # AUTHOR: Jessica Van Os
 # CONTACT: jvanos@ualberta.ca
 # OTHER CONTRIBUTORS:
+<<<<<<< HEAD
+# CREATED: May 2022; LAST EDIT: June 13, 2022
+
+################################################################################
+# LOAD & ATTACH REQUIRED PACKAGES
+{
+  library(odbc)          # Driver for Database Loading
+  library(DBI)           # Package for interface between database and R
+  library(ggplot2)       # Package for graphics and plot aesthetics
+  library(here)          # Package to set filepaths inside R project
+}
+=======
 # LAST EDIT: June 2022
 
 
@@ -16,22 +28,91 @@ library(odbc)          # Driver for Database Loading
 library(DBI)           # Package for interface between database and R
 library(ggplot2)       # Package for graphics and plot aesthetics
 
+>>>>>>> parent of 030a4c7 (Data Files Add & Update Code)
 ################################################################################
 # CONNECT TO MICROSOFT SQL SERVER
-
-#Input Database Name below:
-SourceDB<-"AuroraNetZeroCase1_Jess_May30"
-
-#Connect to database specified (via server, user, and password)
-con <- dbConnect(odbc(),
-                 Driver = "SQL Server",
-                 Server = rstudioapi::askForPassword("Server"),
-                 Database = SourceDB,
-                 UID = rstudioapi::askForPassword("User Name"),
-                 PWD = rstudioapi::askForPassword("Password"))
-
+{
+  #Input Database Name below:
+  SourceDB<-"AuroraNetZeroCase1_Jess_May30"
+  
+  #Connect to database specified (via server, user, and password)
+  con <- dbConnect(odbc(),
+                   Driver = "SQL Server",
+                   Server = rstudioapi::askForPassword("Server"),
+                   Database = SourceDB,
+                   UID = rstudioapi::askForPassword("User Name"),
+                   PWD = rstudioapi::askForPassword("Password"))
+}
 ################################################################################
+<<<<<<< HEAD
+# READ TABLES FROM DATABASE INTO ENVIRONMENT
+# Can edit to select required tables only
+{
+  # Fuel tables
+  FuelYr <- dbReadTable(con,'FuelYear1')
+  FuelMn <- dbReadTable(con,'FuelMonth1')
+  
+  # Resource Tables
+  ResYr <- dbReadTable(con,'ResourceYear1')
+  ResMn <- dbReadTable(con,'ResourceMonth1')
+  ResHr <- dbReadTable(con,'ResourceHour1')
+  
+  # Resource Group Tables
+  ResGroupYr <- dbReadTable(con,'ResourceGroupYear1')
+  ResGroupMn <- dbReadTable(con,'ResourceGroupMonth1')
+  ResGroupHr <- dbReadTable(con,'ResourceGroupHour1')
+  ResGroupEmYr <- dbReadTable(con,'ResourceGroupEmissionsYear1')
+  ResGroupEmSt <- dbReadTable(con,'ResourceGroupEmissionsStudy1')
+  
+  # Other Tables
+  ResStackYr <- dbReadTable(con,'ResourceStackYear1')
+  ResStackHr  <- dbReadTable(con,'ResourceStackHour1')
+  Link <- dbReadTable(con,'LinkYear1')
+#  CC <- dbReadTable(con,'CustomConstraint1')
+  ZoneYr <- dbReadTable(con,'ZoneYear1')
+  ZoneMn <- dbReadTable(con,'ZoneMonth1')
+  ZoneHr <- dbReadTable(con,'ZoneHour1')
+#  LTRes <- dbReadTable(con,'LTResValue1')
+#  BuildReport <- dbReadTable(con,'LTBuildReport1')
+#  Study <- dbReadTable(con,'StudyLog1')
+  
+  # Get rid of Unused R memory
+  gc() 
+}
+################################################################################
+# BRING IN OTHER DATA FROM FILES
+
+# Load Leach Merit Data
+merit <- readRDS(here("Data Files","Leach_MeritData.RData"))
+
+  #Filter Data to relevant dates & remove old
+  merit_filt <- filter(merit, 
+                       date >= as.Date("2020-01-1"))
+  rm(merit)
+
+# Load nrgstream_gen
+load(here("Data Files","nrgstream_gen.RData")) 
+
+  # Rename the time
+  nrgstream_gen <- nrgstream_gen %>% rename(time=Time)
+  
+  # Get rid of blanks
+  nrgstream_gen<-nrgstream_gen[!is.na(nrgstream_gen$gen),] 
+  nrgstream_gen<-nrgstream_gen[!is.na(nrgstream_gen$time),] 
+  
+  # Organize the data
+  demand <- nrgstream_gen %>%
+    group_by(time) %>%
+    summarise(Demand = median(Demand), 
+              Price = median(Price),
+              AIL = median(AIL))
+  sub_samp<-filter(nrgstream_gen, time >= as.Date("2017-01-1"))
+  rm(nrgstream_gen)
+
+
+=======
 # READ IN TABLES FROM DATABASE
+>>>>>>> parent of 030a4c7 (Data Files Add & Update Code)
 
 Link <- dbReadTable(con,'LinkStudy1')
 
---
+++ plot_functions.R
@@ -0,0 +1,11 @@
+################################################################################
+# TITLE: plot_functions
+# DESCRIPTION: Functions to generate plots from simulation and AESO input data
+
+
+# AUTHOR: Jessica Van Os
+# CONTACT: jvanos@ualberta.ca
+# OTHER CONTRIBUTORS:
+# CREATED: June 2022; LAST EDIT: June 13, 2022
+
+################################################################################
