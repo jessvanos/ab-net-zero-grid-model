4f1f32f9561b39df4f5ee895a054c92007ba86da
J Van Os <jvanos@ualberta.ca>
2024-04-26 10:47:52 -0600

    Minor fix to avoid overwriting file names

--- Functions/Group_PlotSave.R
+++ Functions/Group_PlotSave.R
@@ -582,9 +582,9 @@ Ren_Curtail_Gen_save <- function(CaseName) {
   # 4 Months with curtail
   GGSave_Loc_hourly(CaseName,"2023 4 month sum renewable curtail (Stacked Area + price)",FourMonthSummary_rCurtail(2023,01,04,07,10,BC),100)
   GGSave_Loc_hourly(CaseName,"2035 4 month sum renewable curtail (Stacked Area + price)",FourMonthSummary_rCurtail(2035,01,04,07,10,BC),100)
-  GGSave_Loc_hourly(CaseName,"2045 4 month sum renewable curtail (Stacked Area + price)",FourMonthSummary_rCurtail(2045,01,04,07,10,BC),100)
-  GGSave_Loc_hourly(CaseName,"2045 4 month sum renewable curtail (Stacked Area + price)",FourMonthSummary_rCurtail(2045,02,05,08,11,BC),100)
-  GGSave_Loc_hourly(CaseName,"2045 4 month sum renewable curtail (Stacked Area + price)",FourMonthSummary_rCurtail(2045,03,06,09,12,BC),100)
+  GGSave_Loc_hourly(CaseName,"2045 4 month sum renewable curtail (Stacked Area + price) 1b",FourMonthSummary_rCurtail(2045,01,04,07,10,BC),100)
+  GGSave_Loc_hourly(CaseName,"2045 4 month sum renewable curtail (Stacked Area + price) 2b",FourMonthSummary_rCurtail(2045,02,05,08,11,BC),100)
+  GGSave_Loc_hourly(CaseName,"2045 4 month sum renewable curtail (Stacked Area + price) 3b",FourMonthSummary_rCurtail(2045,03,06,09,12,BC),100)
   
   # One week example
   GGSave_Loc_hourly(CaseName,"2035 week renewable curtail simple",Week12_wCURTAIL_simple(2035,06,08,BC),100)
